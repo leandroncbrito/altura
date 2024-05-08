d4a83bc7240bca25284f310f80a3c049f8de27c6
Leandro Nogueira de Carvalho Brito <leandroncbrito@gmail.com>
2024-05-08 11:15:43 +0200

    Initial commit, model

---
+++ Oportunity.cs
@@ -0,0 +1,25 @@
+﻿namespace Altura
+{
+    /*
+     * : Id, TenderId, LotNumber, Deadline,
+         Name, TenderName, ExpirationDate, HasDocuments, Location, PublicationDate, Status,
+         Currency, Value
+     */
+    public class Oportunity
+    {
+        public required Guid Id { get; set; }
+        public required Guid TenderId { get; set; }
+        public string LotNumber { get; set; } = string.Empty;
+        public DateTime? Deadline { get; set; }
+        public required string Name { get; set; }
+        public required string TenderName { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public bool HasDocuments { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public DateTime PublicationDate { get; set; }
+        // TODO: enum?
+        public byte Status { get; set;}
+        public string Currency { get; set; } = string.Empty;
+        public decimal? Value { get; set; }
+    }
+}
